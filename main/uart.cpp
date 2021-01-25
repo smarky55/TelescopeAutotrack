64545aff74c88e3ebe8ee278e52d47626123e4db
Sam Kingdon <smarky55@gmail.com>
2021-01-25 20:45:32 +0000

    UART driver

---
+++ main/uart.cpp
@@ -0,0 +1,114 @@
+#include "uart.hpp"
+
+#include <driver/uart.h>
+
+using namespace UART;
+
+UARTDriver::UARTDriver(Port port, const Config& config) : m_port(port) {
+  setBaudRate(config.baud);
+  setParity(config.parity);
+  setStopBits(config.stopBits);
+  setFlowControl(config.flowControl, config.flowCtrlRxThresh);
+
+  setPins(config.txPin, config.rxPin, config.rtsPin, config.ctsPin);
+
+  install(config.txBufferSize, config.rxBufferSize);
+}
+
+UARTDriver::~UARTDriver() {
+  remove();
+}
+
+void UARTDriver::setBaudRate(int baud) {
+  ESP_ERROR_CHECK(uart_set_baudrate(getImplPort(), baud));
+}
+
+void UARTDriver::setParity(Parity parity) {
+  switch (parity) {
+    case Parity::Disable:
+      ESP_ERROR_CHECK(uart_set_parity(getImplPort(), UART_PARITY_DISABLE));
+      break;
+    case Parity::Even:
+      ESP_ERROR_CHECK(uart_set_parity(getImplPort(), UART_PARITY_EVEN));
+      break;
+    case Parity::Odd:
+      ESP_ERROR_CHECK(uart_set_parity(getImplPort(), UART_PARITY_ODD));
+      break;
+  }
+}
+
+void UARTDriver::setStopBits(StopBits stopBits) {
+  switch (stopBits) {
+    case StopBits::Bits_1:
+      ESP_ERROR_CHECK(uart_set_stop_bits(getImplPort(), UART_STOP_BITS_1));
+      break;
+    case StopBits::Bits_1_5:
+      ESP_ERROR_CHECK(uart_set_stop_bits(getImplPort(), UART_STOP_BITS_1_5));
+      break;
+    case StopBits::Bits_2:
+      ESP_ERROR_CHECK(uart_set_stop_bits(getImplPort(), UART_STOP_BITS_2));
+      break;
+  }
+}
+
+void UARTDriver::setFlowControl(FlowControl ctrl, int rxThresh) {
+  switch (ctrl) {
+    case FlowControl::Disable:
+      ESP_ERROR_CHECK(
+          uart_set_hw_flow_ctrl(getImplPort(), UART_HW_FLOWCTRL_DISABLE, 0));
+      break;
+    case FlowControl::CTS:
+      ESP_ERROR_CHECK(
+          uart_set_hw_flow_ctrl(getImplPort(), UART_HW_FLOWCTRL_CTS, 0));
+      break;
+    case FlowControl::RTS:
+      ESP_ERROR_CHECK(
+          uart_set_hw_flow_ctrl(getImplPort(), UART_HW_FLOWCTRL_RTS, rxThresh));
+      break;
+    case FlowControl::Both:
+      ESP_ERROR_CHECK(uart_set_hw_flow_ctrl(
+          getImplPort(), UART_HW_FLOWCTRL_CTS_RTS, rxThresh));
+      break;
+  }
+}
+
+void UARTDriver::setPins(int txPin, int rxPin, int rtsPin, int ctsPin) {
+  ESP_ERROR_CHECK(uart_set_pin(getImplPort(), txPin, rxPin, rtsPin, ctsPin));
+}
+
+void UARTDriver::install(int txBufferSize, int rxBufferSize) {
+  ESP_ERROR_CHECK(uart_driver_install(getImplPort(), rxBufferSize, txBufferSize,
+                                      10, &m_eventQueue, 0));
+}
+
+void UARTDriver::remove() {
+  if (uart_is_driver_installed(getImplPort())) {
+    ESP_ERROR_CHECK(uart_driver_delete(getImplPort()));
+  }
+}
+
+void UARTDriver::sendBytes(const std::vector<uint8_t>& data) 
+{
+  uart_write_bytes(getImplPort(), (char*)(data.data()), data.size());
+}
+
+void UARTDriver::receiveBytes(std::vector<uint8_t>& data) 
+{
+  size_t length = 0;
+  ESP_ERROR_CHECK(uart_get_buffered_data_len(getImplPort(), &length));
+  data.resize(length);
+  uart_read_bytes(getImplPort(), data.data(), length, 100);
+}
+
+int UARTDriver::getImplPort() const {
+  switch (m_port) {
+    case Port::Port0:
+      return UART_NUM_0;
+    case Port::Port1:
+      return UART_NUM_1;
+    case Port::Port2:
+      return UART_NUM_2;
+    default:
+      return UART_NUM_MAX;
+  }
+}
---
+++ main/uart.hpp
@@ -0,0 +1,74 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+#include <vector>
+
+namespace UART {
+
+enum class Port {
+  Port0,  // This port is usually the UART-USB bridge
+  Port1,
+  Port2
+};
+
+enum class Parity { Disable, Even, Odd };
+
+enum class StopBits { Bits_1, Bits_1_5, Bits_2 };
+
+enum class FlowControl { Disable, RTS, CTS, Both };
+
+enum class ClockSource { APB, RefTick };
+
+constexpr int PIN_NO_CHANGE = -1;
+
+struct Config {
+  int txPin = PIN_NO_CHANGE;
+  int rxPin = PIN_NO_CHANGE;
+  int rtsPin = PIN_NO_CHANGE;
+  int ctsPin = PIN_NO_CHANGE;
+  int baud;
+  Parity parity = Parity::Disable;
+  StopBits stopBits = StopBits::Bits_1;
+  FlowControl flowControl = FlowControl::Disable;
+  int flowCtrlRxThresh = 0;
+  ClockSource clockSource = ClockSource::APB;
+  int txBufferSize = 2048;
+  int rxBufferSize = 2048;
+};
+
+class UARTDriver {
+ public:
+  UARTDriver(Port port, const Config& config);
+  virtual ~UARTDriver();
+
+  void setBaudRate(int baud);
+
+  void setParity(Parity parity);
+
+  void setStopBits(StopBits stopBits);
+
+  void setFlowControl(FlowControl ctrl, int rxThresh);
+
+  void setPins(int txPin = PIN_NO_CHANGE,
+               int rxPin = PIN_NO_CHANGE,
+               int rtsPin = PIN_NO_CHANGE,
+               int ctsPin = PIN_NO_CHANGE);
+
+  void install(int txBufferSize, int rxBufferSize);
+
+  void remove();
+
+  void sendBytes(const std::vector<uint8_t>& data);
+
+  void receiveBytes(std::vector<uint8_t>& data);
+
+ private:
+  const Port m_port;
+  constexpr int getImplPort() const;
+
+  void* m_eventQueue;
+};
+
+}  // namespace UART
+
+#endif  // __UART_H__
