29bdc9d61ae28f9ca3b75663de31ae17ba6dd8d7
Sam Kingdon <smarky55@gmail.com>
2021-01-30 23:27:29 +0000

    Upgrade html with slider

--- main/scope.js
+++ main/scope.js
@@ -1,3 +1,69 @@
+function panControl(id, onUpdate) {
+  var range = document.getElementById(id);
+  var handle = range.children[0];
+  var down = false;
+  var rangeWidth = range.offsetWidth;
+  var rangeLeft = range.offsetWidth;
+  var handleWidth = handle.offsetWidth;
+
+  range.addEventListener("mousedown", function (e) {
+    rangeWidth = this.offsetWidth;
+    rangeLeft = this.offsetLeft;
+    down = true;
+    updateHandle(e);
+    e.preventDefault();
+    return false;
+  });
+
+  document.addEventListener("mousemove", function (e) {
+    e.preventDefault()
+    updateHandle(e);
+  })
+
+  document.addEventListener("mouseup", function (e) {
+    resetHandle();
+    down = false;
+  })
+
+  function updateHandle(e) {
+    if (down) {
+      handle.style.left = Math.max(Math.min(e.pageX - rangeLeft - handleWidth / 2, rangeWidth - handleWidth), 0) + "px";
+      if (typeof onUpdate == "function") onUpdate(Math.round(((e.pageX - rangeLeft) / rangeWidth) * 100));
+    }
+  }
+
+  function resetHandle() {
+    handle.style.left = ((rangeWidth - handleWidth) / 2) + "px";
+    if (down && typeof onUpdate == "function") {
+      onUpdate(50);
+    }
+  }
+
+  resetHandle();
+}
+
+window.onload = function () {
+
+  // var slider = document.getElementById("slider");
+  var output = document.getElementById("output");
+
+  panControl("slider", function (val) {
+    output.innerHTML = val
+
+    const data = {
+      command: "speed",
+      value: (val * 2) - 100
+    };
+    fetch("/command", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+  });
+}
+
 function buttonClick(element) {
   const data = { button: element.id };
   fetch("/command", {
